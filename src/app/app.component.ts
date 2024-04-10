99360630ca39855edffb4aafb86eb0d5542cb084
Julian Bayer <jdbayer09@hotmail.com>
2024-04-10 17:06:42 -0500

    configuracion inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: '',
+  styles: ''
 })
 export class AppComponent {
-  title = 'cuentas';
+
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  api_url: '#{serverUrl}#',
+  webSocket_url: '#{webSocketUrl}#',
+  encrypt_data_key: '#{encryptDataKey}#'
+};
