13dfb6901b8fce22c7795d3dc136e3bf44c45163
Julian Bayer <jdbayer09@hotmail.com>
2024-04-12 19:31:24 -0500

    correccion de modulos

--- src/app/pages/public/public-routing.module.ts
+++ src/app/pages/public/public-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PublicComponent } from './public.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: PublicComponent,
     children: [
       {
         path: '',
--- src/app/pages/zone/zone-routing.module.ts
+++ src/app/pages/zone/zone-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ZoneComponent } from './zone.component';
 
-const routes: Routes = [{ path: '', component: ZoneComponent }];
+const routes: Routes = [{ path: '' }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
