eda4b426796c912916acc54037a202a48023b180
Julian Bayer <jdbayer09@hotmail.com>
2024-04-14 18:09:09 -0500

    Configuracion de rutas publicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { authGuard, noAuthGuard } from './guards/auth';
+import { authGuard } from './guards/auth';
 
 const routes: Routes = [
   {
@@ -10,8 +10,7 @@ const routes: Routes = [
   },
   {
     path: 'p',
-    loadChildren: () => import('./pages/public/public.module').then(m => m.PublicModule),
-    canActivate: [noAuthGuard]
+    loadChildren: () => import('./pages/public/public.module').then(m => m.PublicModule)
   },
   {
     path: 'z',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, EffectRef, Signal, computed, effect, inject } from '@angular/core';
+import { Component, Signal, computed, inject } from '@angular/core';
 import { AuthService } from './services/security/auth.service';
-import { Router } from '@angular/router';
 import { AuthStatus } from './enums';
 
 @Component({
@@ -21,7 +20,6 @@ import { AuthStatus } from './enums';
 export class AppComponent {
 
   private authSV = inject( AuthService );
-  private router = inject( Router );
 
   finishedAuthCheck:Signal<boolean> = computed<boolean>(() => {
     if ( this.authSV.authStatus() === AuthStatus.checking ) {
@@ -29,18 +27,4 @@ export class AppComponent {
     }
     return true;
   });
-
-  private authStatusChangedEffect: EffectRef = effect(() => {
-    switch( this.authSV.authStatus() ) {
-      case AuthStatus.checking:
-        return;
-      case AuthStatus.authenticated:
-        this.router.navigateByUrl('/z/dashboard', {replaceUrl: true});
-        return;
-      case AuthStatus.notAuthenticated:
-        this.router.navigateByUrl('/p/login', {replaceUrl: true});
-        return;
-    }
-  });
-
 }
