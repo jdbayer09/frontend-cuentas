ae6aa89947bfe1ffb7addf0d6852e46665a51991
Julian Bayer <jdbayer09@hotmail.com>
2024-04-16 18:54:31 -0500

    Cambio para el idioma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { registerLocaleData } from '@angular/common';
+import localeEsCO from '@angular/common/locales/es-CO';
+
+registerLocaleData(localeEsCO);
 
 @NgModule({
   declarations: [
@@ -17,7 +22,10 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   providers: [
-
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-co'
+    }
   ],
   bootstrap: [
     AppComponent
