dbc54ebb695f8248ffa84e0a5bc02641dd69062f
Julian Bayer <jdbayer09@hotmail.com>
2024-04-16 17:58:18 -0500

    Configuracion para consulta de categorias

--- src/app/pages/zone/categories/categories.component.ts
+++ src/app/pages/zone/categories/categories.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { CategoriesService } from '../../../services/categories/categories.service';
+import { CategoryResponse } from '../../../interfaces/categories';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit{
+  //! Inyecciones
+  private categorySV = inject(CategoriesService);
+  //! -----------------------------------------------
 
+  //* Señales
+  private _loading = signal<boolean>(false);
+  loading = computed<boolean>(() => this._loading());
+
+  private _error = signal<string | null >(null);
+  error = computed<string | null>(() => this._error());
+
+  private _listCategories = signal<CategoryResponse[]>([]);
+  listCategories = computed<CategoryResponse[]>(() => this._listCategories());
+  //*------------------------------------------------
+
+  ngOnInit(): void {
+    this.loadListCategories();
+  }
+
+  private loadListCategories() {
+    this._loading.set(true);
+    this._error.set(null);
+    setTimeout(() => {
+      this.categorySV.listAllCategories().subscribe({
+        next: resp => {
+          this._loading.set(false);
+          this._listCategories.set(resp);
+        },
+        error: err => {
+          this._error.set(err);
+          this._loading.set(false);
+        }
+      });
+    }, 500);
+  }
 }
--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Signal, computed, inject } from '@angular/core';
+import { UtilService } from '../util/util.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable, catchError, throwError } from 'rxjs';
+import { CategoryResponse } from '../../interfaces/categories';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
+  //! Inyecciones
+  private utilSV = inject(UtilService);
+  private http = inject(HttpClient);
+  //! -----------------------------------------------------------------
 
-  constructor() { }
+  //? Variables
+  private readonly baseUrl: string = environment.api_url + '/category';
+  //? -----------------------------------------------------------------
+
+  //* Señales
+  private httpHeaders: Signal<HttpHeaders> = computed( () => this.utilSV.getHeaders() );
+  //*------------------------------------------------------------------
+
+  listAllCategories(): Observable<CategoryResponse[]> {
+    const url  = `${ this.baseUrl }/list-all`;
+    return this.http.get<CategoryResponse[]>( url, {headers: this.httpHeaders()} )
+      .pipe(
+        catchError( err => throwError( () => err.error.errorMessage ))
+      );
+  }
 }
