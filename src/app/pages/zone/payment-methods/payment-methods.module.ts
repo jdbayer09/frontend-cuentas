a9d8c4d9260e08575aba9eef4def4fe54f47aea4
Julian Bayer <jdbayer09@hotmail.com>
2024-04-23 10:01:02 -0500

    Configuracion de pagina completa

--- src/app/pages/zone/payment-methods/payment-methods.module.ts
+++ src/app/pages/zone/payment-methods/payment-methods.module.ts
@@ -3,6 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { PaymentMethodsRoutingModule } from './payment-methods-routing.module';
 import { PaymentMethodsComponent } from './payment-methods.component';
+import { PaymentMethodModalModule } from '../../../modals/payment-method-modal/payment-method-modal.module';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TooltipModule } from 'primeng/tooltip';
 
 
 @NgModule({
@@ -11,7 +16,12 @@ import { PaymentMethodsComponent } from './payment-methods.component';
   ],
   imports: [
     CommonModule,
-    PaymentMethodsRoutingModule
+    PaymentMethodsRoutingModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    TooltipModule,
+    PaymentMethodModalModule
   ]
 })
 export class PaymentMethodsModule { }
