9c6a2bfd37e45ec40f72ccc2ebe1b42d6730f91d
Julian Bayer <jdbayer09@hotmail.com>
2024-04-23 09:28:29 -0500

    Configuracion de interfaces

---
+++ src/app/interfaces/paymentMethods/index.ts
@@ -0,0 +1,3 @@
+export { BasePaymentMethod } from './basePaymentMethod.interface';
+export { PaymentMethod } from './paymentMethod.interface';
+export { PaymentMethodRequest } from './paymentMethodRequest.interface';
---
+++ src/app/interfaces/paymentMethods/paymentMethod.interface.ts
@@ -0,0 +1,9 @@
+export interface PaymentMethod {
+  id:          number;
+  name:        string;
+  color:       string;
+  icon:        string;
+  description: string;
+  active:      boolean;
+  createdAt:   Date;
+}
