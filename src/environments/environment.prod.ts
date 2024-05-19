190367f6f3c0a6999226968ce4f818edfd68c473
Julian Bayer <jdbayer09@hotmail.com>
2024-05-19 13:39:42 -0500

    URL para servidor de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
-  api_url: '#{serverUrl}#',
-  webSocket_url: '#{webSocketUrl}#',
-  encrypt_data_key: '#{encryptDataKey}#'
+  api_url: 'http://ec2-54-226-162-224.compute-1.amazonaws.com:808/api',
+  public: {
+    tittle: 'CUENTAS PERSONALES',
+    info: 'Una aplicación de gestión de cuentas personales intuitiva y segura que ayuda a los usuarios a administrar sus finanzas de manera eficiente. Permite realizar seguimiento de gastos, presupuestos, ahorros y transacciones, ofreciendo herramientas para un control total y una planificación financiera efectiva, todo en un solo lugar.'
+  }
 };
+
+
+
